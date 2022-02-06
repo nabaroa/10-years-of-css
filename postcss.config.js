d6ad4eac6ba26ed61900f943a133b45cbc213999
Naiara Abaroa <naiaraabaroa@gmail.com>
2022-02-06 11:02:08 +0100

    Remove gulp dependency

---
+++ postcss.config.js
@@ -0,0 +1,18 @@
+require('postcss-import');
+require('postcss-preset-env');
+require('cssnano');
+
+module.exports = {
+  plugins: {
+    'postcss-import':{},
+    'postcss-preset-env': {
+      stage: 1,
+      features: {
+        'custom-media-queries': true,
+      }
+    },
+    cssnano: {
+      preset: 'default',
+    },
+  },
+};
